2d5fd40d0663da9cd4f8eaa7d05ce887398ad6ff
lightwingzz <alvin.nico@gmail.com>
2020-06-08 22:37:59 +0700

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const countrylists = require("./routes/countrylist");
 const nearestcitys = require("./routes/nearestcity");
 const latitudelongitudes = require("./routes/latitudelongitude");
+const statelists = require("./routes/statelist");
 const app = express();
 
 app.use(cors());
@@ -10,10 +11,12 @@ app.use(express.urlencoded({extended:true}));
 app.use("/api/countrylist",countrylists);
 app.use("/api/nearestcity",nearestcitys);
 app.use("/api/latitudelongitude",latitudelongitudes);
+app.use("/api/statelist",statelists);
 
 app.listen(3000, function(){
     console.log("Listening to port 3000");
 });
+
 //iqair.com
 //airvisual
 //key
--- routes/statelist.js
+++ routes/statelist.js
@@ -2,28 +2,14 @@ const express = require("express");
 const router = express.Router();
 var request = require('request');
 
-/*
-require('dotenv').config();
-
-const pool = mysql.createPool(
-    {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-    }
-)
-*/
-
-function getnearestcity(nearestcity)
+function getstatelist(statelist)
 {
     return new Promise(function(resolve,reject)
     {
         var options = 
         {
             'method': 'GET',
-            'url': `https://api.airvisual.com/v2/nearest_city?key=e00e72e8-d53b-40ba-9cae-cb9a21f88fd3`
+            'url': `https://api.airvisual.com/v2/states?country=usa&key=e00e72e8-d53b-40ba-9cae-cb9a21f88fd3`
         };
     
         request(options, function(error,response)
@@ -37,7 +23,7 @@ function getnearestcity(nearestcity)
 router.get("/", async function(req, res){
     try
     {
-        res.status(200).send(await getnearestcity(req.query.nearestcity));
+        res.status(200).send(await getstatelist(req.query.statelist));
     }
 
     catch (error)
